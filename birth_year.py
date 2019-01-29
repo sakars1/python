689c7ff956db835a00a89958d3dc89d26d3a0190
NTC-PC\NTC <rakasidebus@gmail.com>
2019-01-29 23:07:35 +0545

    birtyear

---
+++ birth_year.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+current_year = int(input("Enter current year: "))
+
+print("Hello ", name, " your birth year is: ", current_year-age)
+
