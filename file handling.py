c0dd61db44b91404a21a29545c4e88572c424e5b
sakars1 <rakasidebus@gmail.com>
2019-02-12 08:00:52 +0545

    file handling

--- file handling.py
+++ file handling.py
@@ -1,7 +1,15 @@
 # f=open('file.txt', 'w')
 # f.writelines('Hello world from file ')
 
-with open("file.txt", "r") as f:
-    text = f.read()
+# with open("file.txt", "r") as f:
+#     text = f.read()
+#
+# print(text)
 
-print(text)
+f=open('file.txt', 'w')
+text=input("write to the file.... write quit to stop")
+while text!='quit':
+    f.writelines(text+'\n')
+    text=input()
+print("done with writing now saving...")
+f.close()
